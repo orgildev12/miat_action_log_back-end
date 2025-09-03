e93cf1948d21ff836e808dcb3636831d92f92d27
Orgil Batsukh <124234225+orgildev12@users.noreply.github.com>
2025-09-03 19:20:01 +0800

    small change

--- services/alogTestService.ts
+++ services/alogTestService.ts
@@ -5,45 +5,31 @@ import { AlogTestQueries } from './alogTestQueries';
 export class AlogTestService {
   async getAllRecords(): Promise<oracledb.Result<any>> {
     return await dbManager.executeQuery(
-      AlogTestQueries.getAllRecords, 
-      []
+      AlogTestQueries.getAllRecords, []
     );
   }
 
-  async createRecord(
-    name: string
-  ): Promise<oracledb.Result<any>> {
+  async createRecord(name: string): Promise<oracledb.Result<any>> {
     return await dbManager.executeQuery(
-      AlogTestQueries.createRecord, 
-      [name]
+      AlogTestQueries.createRecord, [name]
     );
   }
 
-  async getRecordById(
-    id: string
-  ): Promise<oracledb.Result<any>> {
+  async getRecordById(id: string): Promise<oracledb.Result<any>> {
     return await dbManager.executeQuery(
-      AlogTestQueries.getRecordById, 
-      [id]
+      AlogTestQueries.getRecordById, [id]
     );
   }
 
-  async updateRecord(
-    id: string, 
-    name: string
-  ): Promise<oracledb.Result<any>> {
+  async updateRecord(id: string, name: string): Promise<oracledb.Result<any>> {
     return await dbManager.executeQuery(
-      AlogTestQueries.updateRecord, 
-      [name, id]
+      AlogTestQueries.updateRecord, [name, id]
     );
   }
 
-  async deleteRecord(
-    id: string
-  ): Promise<oracledb.Result<any>> {
+  async deleteRecord(id: string): Promise<oracledb.Result<any>> {
     return await dbManager.executeQuery(
-      AlogTestQueries.deleteRecord, 
-      [id]
+      AlogTestQueries.deleteRecord, [id]
     );
   }
 }
